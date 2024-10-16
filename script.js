f4b433f1432e6f0ce62b6bac2ef5fb5374fefdac
fralone200 <165789100+fralone200@users.noreply.github.com>
2024-10-16 09:37:01 -0300

    java script 3

---
+++ script.js
@@ -0,0 +1,11 @@
+letValorContador = 0;
+
+function incrementar(){
+    ValorContador++;
+    document.getElementByld('contador').innerText = ValorContador;
+}
+
+function diminuir(){
+    ValorContador--;
+    document.getElementByld('contador').innerText = ValorContador;
+}
